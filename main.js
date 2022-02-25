9a00b36d47e9bbf9f3f8de2277489bc977fe5b89
victor balladares <victor.m.balladares.f@gmail.com>
2022-02-25 10:59:44 +0100

    estructura

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Practicaremos cómo crear gráficas con las librerías vistas en clase
+
+Pediremos las películas de Star Wars y pintaremos una gráfica de líneas en la que podamos ver cada una de las películas.
+En el eje X el nombre de la película
+En el eje Y año de publicación
+
+*/
